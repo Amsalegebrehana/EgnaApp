de904852e48b0e09e4584fefff3916bf2779c9b5
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2020-10-14 05:13:26 -0700

    First commit

---
+++ app/src/main/java/com/example/egnaleegna/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.egnaleegna;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    float x1, x2,y1,y2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1<x2){
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }else if(x1 > x2){
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+            break;
+        }
+        return false;
+    }
+
+
+}
